d3564e260c13e3a1727f7663077acbe752f4c14e
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-26 19:48:42 +0300

    fmt and const panic

--- src/panics.rs
+++ src/panics.rs
@@ -7,18 +7,30 @@ pub(crate) fn position_not_on_char_boundary(pos: usize) -> ! {
 #[cold]
 #[track_caller]
 #[inline(never)]
-pub(crate) fn oob_read() -> ! {
+pub(crate) const fn oob_read() -> ! {
     panic!("index for gap insertion is out of bounds");
 }
 
 #[cold]
 #[track_caller]
 #[inline(never)]
-pub(crate) fn invalid_offset(len: usize, src_offset: usize, dst_offset: usize, copy_count: usize) -> ! {
+pub(crate) fn invalid_offset(
+    len: usize,
+    src_offset: usize,
+    dst_offset: usize,
+    copy_count: usize,
+) -> ! {
     panic!(
         "pointers should never overlap when copying, \
                 len is {}, source pointer offset is {}, destination \
                 pointer offset is {} with a copy count of {}",
         len, src_offset, dst_offset, copy_count
     );
+}
+
+#[cold]
+#[track_caller]
+#[inline(never)]
+pub(crate) const fn invlaid_max_gap_size() -> ! {
+    panic!("invalid max gap size");
 }
