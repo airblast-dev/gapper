8ff7c49316abd5eac442c31facf6e4cf0728579d
Tayfun Bocek <tayfunbocek@live.ca>
2025-01-29 23:03:45 +0300

    cleanup and get methods for GapString

--- src/gapstr.rs
+++ src/gapstr.rs
@@ -1,4 +1,9 @@
-use crate::{raw_gap_buf::RawGapBuf, utils::get_parts_at};
+use std::{ops::RangeBounds, str::from_utf8_unchecked};
+
+use crate::{
+    raw_gap_buf::RawGapBuf,
+    utils::{get_parts_at, get_range, u8_is_char_boundary},
+};
 
 const DEFAULT_GAP_SIZE: usize = 4096;
 
@@ -16,10 +21,14 @@ impl GapString {
     }
 
     fn insert(&mut self, s: &str, at: usize) {
+        let at_byte = self.raw.get(at);
+
+        // The insert position must be at a char boundary or at the end.
+        assert!(at == self.raw.len() || at_byte.copied().is_some_and(u8_is_char_boundary));
         let gap_len = self.raw.gap_len();
         let s_bytes = s.as_bytes();
         if s.len() > gap_len {
-            let [start, end] = self.raw.get_slices();
+            let [start, end] = self.raw.get_parts();
             let (start, mid, end, before_mid) = get_parts_at(start, end, at);
             let new_raw = if before_mid {
                 RawGapBuf::new_with_slice([start, s_bytes], DEFAULT_GAP_SIZE, [mid, end])
@@ -39,6 +48,33 @@ impl GapString {
         unsafe { spare.copy_from_nonoverlapping(s_ptr, s.len()) };
         unsafe { self.raw.grow_start(s.len()) };
     }
+
+    #[inline]
+    fn get<RB: RangeBounds<usize>>(&self, r: RB) -> Option<[&str; 2]> {
+        let r = get_range(self.raw.len(), r)?;
+        if !self.is_get_char_boundary(r.start, r.end) {
+            return None;
+        }
+
+        let [start, end] = self.raw.get_slice(r)?;
+        // SAFETY: we return early if the positions are not on a char boundary the slices are now
+        // guaranteed valid UTF-8 encoded bytes
+        unsafe { Some([from_utf8_unchecked(start), from_utf8_unchecked(end)]) }
+    }
+
+    #[inline(always)]
+    pub const fn get_slices(&self) -> [&str; 2] {
+        let [start, end] = self.raw.get_parts();
+        // SAFETY: the gap is always on a char boundary, as such the slice returned is guaranteed
+        // to be valid UTF-8 encoded bytes
+        unsafe { [from_utf8_unchecked(start), from_utf8_unchecked(end)] }
+    }
+
+    #[inline(always)]
+    pub fn is_get_char_boundary(&self, start: usize, end: usize) -> bool {
+        self.raw.get(start).is_some_and(|b| u8_is_char_boundary(*b))
+            && (self.raw.get(end).is_some_and(|b| u8_is_char_boundary(*b)) || self.raw.len() == end)
+    }
 }
 
 #[cfg(test)]
@@ -49,6 +85,12 @@ mod tests {
     fn insert() {
         let mut gap_str = GapString::new();
         gap_str.insert("Hello", 0);
-        assert_eq!(gap_str.raw.get_slices(), [b"Hello".as_slice(), b""])
+        assert_eq!(gap_str.get_slices(), ["Hello", ""]);
+
+        gap_str.insert("Bye", 3);
+        assert_eq!(gap_str.get_slices(), ["HelBye", "lo"]);
+
+        gap_str.insert("123", 7);
+        assert_eq!(gap_str.get_slices(), ["HelByel123", "o"]);
     }
 }
