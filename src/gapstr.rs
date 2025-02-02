01efe16285c362f97676ef1584130cdea201e3e1
Tayfun Bocek <tayfunbocek@live.ca>
2025-02-02 11:34:16 +0300

    redo GapString

--- src/gapstr.rs
+++ src/gapstr.rs
@@ -1,126 +1,154 @@
 use std::{
-    ops::RangeBounds,
+    ops::{Range, RangeBounds},
+    ptr::NonNull,
     str::{from_utf8, from_utf8_unchecked},
 };
 
 use crate::{
+    grower::Grower,
     raw_gap_buf::RawGapBuf,
-    utils::{get_parts_at, get_range, u8_is_char_boundary},
+    utils::{get_range, u8_is_char_boundary},
 };
 
-const DEFAULT_GAP_SIZE: usize = 4096;
-
 #[derive(Clone)]
-pub struct GapString {
-    raw: RawGapBuf<u8>,
+struct GrowingGapString<G: Grower<str>> {
+    buf: RawGapBuf<u8>,
+    grower: G,
 }
 
-impl GapString {
+impl<G: Grower<str>> GrowingGapString<G> {
     #[inline]
-    pub const fn new() -> Self {
+    pub fn new() -> Self
+    where
+        G: Default,
+    {
         Self {
-            raw: RawGapBuf::new(),
+            buf: RawGapBuf::new(),
+            grower: Default::default(),
         }
-    }
-
-    fn insert(&mut self, s: &str, at: usize) {
-        let at_byte = self.raw.get(at);
-
-        // The insert position must be at a char boundary or at the end.
-        assert!(at == self.raw.len() || at_byte.copied().is_some_and(u8_is_char_boundary));
-        let gap_len = self.raw.gap_len();
-        let s_bytes = s.as_bytes();
-        if s.len() > gap_len {
-            let [start, end] = self.raw.get_parts();
-            let (start, mid, end, before_mid) = get_parts_at(start, end, at);
-            let new_raw = if before_mid {
-                // TODO: use realloc methods like on GrowingGapBuf
-                unsafe {
-                    RawGapBuf::new_with_slice(&[start, s_bytes], DEFAULT_GAP_SIZE, &[mid, end])
-                }
-            } else {
-                unsafe {
-                    RawGapBuf::new_with_slice(&[start, mid, s_bytes], DEFAULT_GAP_SIZE, &[end])
-                }
-            };
-            self.raw = new_raw;
-            return;
-        }
-
-        self.raw.move_gap_start_to(at);
-        let spare = self.raw.spare_capacity_mut().cast::<u8>().as_ptr();
-        let s_ptr = s.as_bytes().as_ptr();
-
-        // The function requires that the provided slice does not overlap
-        // it is safe to copy non overlapping
-        unsafe { spare.copy_from_nonoverlapping(s_ptr, s.len()) };
-        unsafe { self.raw.grow_start(s.len()) };
     }
 
     #[inline]
-    fn get<RB: RangeBounds<usize>>(&self, r: RB) -> Option<[&str; 2]> {
-        let r = get_range(self.raw.len(), r)?;
-        if !self.is_get_char_boundary(r.start, r.end) {
+    pub fn with_grower(grower: G) -> Self {
+        Self {
+            buf: RawGapBuf::new(),
+            grower,
+        }
+    }
+
+    #[inline]
+    pub fn from_slices(start: &[&str], gap_size: usize, end: &[&str]) -> Self
+    where
+        G: Default,
+    {
+        let grower = G::default();
+        Self {
+            buf: unsafe {
+                RawGapBuf::new_with_slice(
+                    core::mem::transmute::<&[&str], &[&[u8]]>(start),
+                    gap_size,
+                    core::mem::transmute::<&[&str], &[&[u8]]>(end),
+                )
+            },
+            grower,
+        }
+    }
+
+    #[inline]
+    pub fn get<RB: RangeBounds<usize>>(&self, r: RB) -> Option<[&str; 2]> {
+        let r = get_range(self.buf.len(), r)?;
+        if !self.is_get_char_boundary(r.start..r.end) {
             return None;
         }
 
-        let [start, end] = self.raw.get_range(r)?;
-        // SAFETY: we return early if the positions are not on a char boundary the slices are now
-        // guaranteed valid UTF-8 encoded bytes
-        debug_assert!(from_utf8(start).is_ok());
-        debug_assert!(from_utf8(end).is_ok());
+        let [start, end] = self.buf.get_range(r)?;
         unsafe { Some([from_utf8_unchecked(start), from_utf8_unchecked(end)]) }
     }
 
-    #[inline(always)]
-    pub const fn get_parts(&self) -> [&str; 2] {
-        let [start, end] = self.raw.get_parts();
-        // SAFETY: the gap is always on a char boundary, as such the slice returned is guaranteed
-        // to be valid UTF-8 encoded bytes
-        debug_assert!(from_utf8(start).is_ok());
-        debug_assert!(from_utf8(end).is_ok());
-        unsafe { [from_utf8_unchecked(start), from_utf8_unchecked(end)] }
-    }
-
-    /// Returns a string slice by moving the gap
-    ///
-    /// Calling this method will move the gap to the start or to the end depending on which one
-    /// would need less copies. Because of the performance implications [`GapString::get`], or
-    /// [`GapString::get_parts`] should be preferred wherever it is possible.
-    ///
-    /// If you are calling this in a common code path you should just use a [`String`], or you
-    /// should rethink how you are approaching the problem. Calling this repeatedly mostly defeats
-    /// the purpose of a gap buffer.
-    #[inline(always)]
-    #[allow(clippy::wrong_self_convention)]
-    pub fn to_str(&mut self) -> &str {
-        let s = self.raw.to_slice();
-
-        // SAFETY: all bytes must be valid UTF-8 which is checked when modifying the buffer
-        unsafe { from_utf8_unchecked(s) }
+    #[inline]
+    pub fn get_slice<RB: RangeBounds<usize>>(&mut self, r: RB) -> Option<&str> {
+        let r = get_range(self.buf.len(), r)?;
+        if !self.is_get_char_boundary(r.start..r.end) {
+            return None;
+        }
+        let s = self.buf.get_slice(r)?;
+        unsafe { Some(from_utf8_unchecked(s)) }
     }
 
     #[inline(always)]
-    pub fn is_get_char_boundary(&self, start: usize, end: usize) -> bool {
-        self.raw.get(start).is_some_and(|b| u8_is_char_boundary(*b))
-            && (self.raw.get(end).is_some_and(|b| u8_is_char_boundary(*b)) || self.raw.len() == end)
+    pub fn get_parts(&self) -> [&str; 2] {
+        self.buf
+            .get_parts()
+            .map(|s| unsafe { from_utf8_unchecked(s) })
+    }
+
+    #[inline(always)]
+    fn is_get_char_boundary(&self, r: Range<usize>) -> bool {
+        self.buf
+            .get(r.start)
+            .copied()
+            .is_some_and(u8_is_char_boundary)
+            && (self.buf.get(r.end).is_none()
+                || self
+                    .buf
+                    .get(r.end)
+                    .copied()
+                    .is_some_and(u8_is_char_boundary))
+    }
+
+    pub fn insert(&mut self, s: &str, at: usize) {
+        assert!(
+            self.buf.get(at).copied().is_some_and(u8_is_char_boundary) || self.buf.len() == at,
+            "insertion should always be on a char boundary"
+        );
+        let [start, end] = self
+            .buf
+            .get_parts()
+            .map(|s| unsafe { from_utf8_unchecked(s) });
+        let base_gap_size = self.grower.base_gap_size(start, end);
+        if self.buf.gap_len() < s.len() {
+            self.buf.grow_gap(base_gap_size + s.len());
+        }
+        self.buf.move_gap_start_to(at);
+
+        unsafe {
+            // SAFETY: the references do not overlap and both are correctly aligned
+            // we have allocated enough space above
+            self.buf
+                .spare_capacity_mut()
+                .cast::<u8>()
+                .copy_from_nonoverlapping(NonNull::from(s).cast::<u8>(), s.len());
+            // SAFETY: we have initialized the gaps first s.len items it is now safe to grow the
+            // start
+            self.buf.grow_start(s.len());
+        };
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::GapString;
+    use rstest::rstest;
+    use rstest_reuse::apply;
 
-    #[test]
-    fn insert() {
-        let mut gap_str = GapString::new();
-        gap_str.insert("Hello", 0);
-        assert_eq!(gap_str.get_parts(), ["Hello", ""]);
+    use crate::grower::test_utils::*;
 
-        gap_str.insert("Bye", 3);
-        assert_eq!(gap_str.get_parts(), ["HelBye", "lo"]);
+    use super::GrowingGapString;
 
-        gap_str.insert("123", 7);
-        assert_eq!(gap_str.get_parts(), ["HelByel123", "o"]);
+    #[apply(grower_template)]
+    fn insert(g: TestGrower) {
+        let mut s_buf = GrowingGapString::with_grower(g);
+        s_buf.insert("Hi", 0);
+        assert_eq!(s_buf.get_parts(), ["Hi", ""]);
+        s_buf.insert("AA", 1);
+        assert_eq!(s_buf.get_parts(), ["HAA", "i"]);
+        s_buf.insert("bb", 0);
+        assert_eq!(s_buf.get_parts(), ["bb", "HAAi"]);
+    }
+
+    #[apply(grower_template)]
+    #[should_panic]
+    fn insert_panics(g: TestGrower) {
+        let mut s_buf = GrowingGapString::with_grower(g);
+        s_buf.insert("Hi", 1);
     }
 }
